61a7ed0f8759e470032e5fa11d4c46c7a9804397
Rishijha12 <rishijha937@gmail.com>
2021-12-27 19:46:32 +0530

    Initial Commit

---
+++ ThetaPractical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThetaPractical
+//
+//  Created by Rishi Jha on 27/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
